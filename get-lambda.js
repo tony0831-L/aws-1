3148a23183812434d77237f8ea8477d53f83d513
tony0831-l <t0983822437@gmail.com>
2021-11-02 17:29:31 +0800

    crud

--- get-lambda.js
+++ get-lambda.js
@@ -1,7 +1,31 @@
 const AWS = require('aws-sdk');
 let dynamodb = new AWS.DynamoDB.DocumentClient();
+let date = new Date();
+let now = date.toISOString();
 exports.handler = async (event) => {
-    let resData = await dynamodb.scan({TableName: 'HelloWorldDatabase'}).promise();
+    let resData;
+    switch (event.methods) {
+        case 'get':
+            resData = await dynamodb.scan({TableName: 'HelloWorldDatabase'}).promise();
+            break;
+    
+        case 'post':
+            let name = (event.firstName + event.lastName)
+            let params = {
+                TableName: 'HelloWorldDatabase',
+                Item: {
+                    'id': name,
+                    'LatestGreetingTime': now
+                }
+            };
+            resData = event.weight / ((event.height / 100) * (event.height / 100))
+            await dynamodb.put(params).promise();
+            break;
+
+        case 'del':
+            resData = await dynamodb.delete({TableName: 'HelloWorldDatabase',Key: {id: event.item}}).promise();
+            break;
+    }
     const response = {
         statusCode: 200,
         body: resData
--- index.js
+++ index.js
@@ -8,17 +8,20 @@ Vue.createApp({
             res:0,
             date:dayjs().format('YYYY/MM/DD'),
             time:dayjs().format('HH:mm:ss'),
+            history:[],
         }
     },
     methods:{
         post(){
             this.res="pending....,pleas wait."
+            this.get()
             fetch("https://bdth6z6245.execute-api.us-west-2.amazonaws.com/dev", {
                 method: 'POST',
                 headers: {
                     "Content-Type":"application/json"
                 },
                 body:JSON.stringify({
+                    "methods": 'post',
                     "firstName": this.first,
                     "lastName": this.last ,
                     "height":this.high,
@@ -28,8 +31,46 @@ Vue.createApp({
             .then(response => response.text())
             .then(result => {
                 this.res=this.first+" "+this.last+". your bmi is:"+Math.round(parseFloat(JSON.parse(result).body)*100)/100;
+                this.get()
             })
             .catch(error => console.log('error', error));
-        }
+        },
+        get(){
+            fetch("https://bdth6z6245.execute-api.us-west-2.amazonaws.com/dev", {
+                method: 'POST',
+                headers: {
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify({
+                    "methods": 'get',
+                }),
+            })
+            .then(response => response.text())
+            .then(result => {
+                this.history = (JSON.parse(result).body).Items
+            })
+            .catch(error => console.log('error', error));
+        },
+        del(id){
+            fetch("https://bdth6z6245.execute-api.us-west-2.amazonaws.com/dev", {
+                method: 'POST',
+                headers: {
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify({
+                    "methods": 'del',
+                    "item":id
+                }),
+            })
+            .then(response => response.text())
+            .then(result => {
+                console.log(JSON.parse(result).body)
+                this.get()
+            })
+            .catch(error => console.log('error', error));
+        },
+    },
+    mounted(){
+        this.get()
     }
 }).mount('#app')
